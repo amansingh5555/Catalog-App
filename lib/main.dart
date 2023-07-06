5ddf4c513aa5e41c940a6902933f0865dc8a64f9
amansingh5555 <serijinakhosla@gmail.com>
2023-07-06 11:11:50 +0530

    day2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firstproject/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -8,10 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Container(
-        child: Text('Welcome to 30 days of flutter'),
-      ),
+      home:HomePage(),
     );
   }
 }
