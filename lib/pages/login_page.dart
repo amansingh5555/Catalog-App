348a7458da8bdc5c94a7952bc2614c66bfdbf958
amansingh5555 <serijinakhosla@gmail.com>
2023-07-16 16:35:26 +0530

    Day 7 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,66 +8,93 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  final _formKey = GlobalKey<FormState>();
+  moveTOHome(BuildContext context){}
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              "assests/image/login_page.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text (
-              "Welcome $name",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                "assests/image/login_page.png",
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16,horizontal:32 ),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter UserName",
-                      labelText: "UserName",
-                    ),
-                    onChanged: (value){
-                      name = value ;
-                      setState(() {
+              SizedBox(
+                height: 20.0,
+              ),
+              Text (
+                "Welcome $name",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16,horizontal:32 ),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter UserName",
+                        labelText: "UserName",
+                      ),
+                      validator: (value){
+                        if(value != null && value.isEmpty){
+                          return "UserName cannot be empty";
+                        }
+                        return null;
+                      },
 
-                      });
-                    },
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter PassWord",
-                      labelText: "Password",
+                      onChanged: (value){
+                        name = value ;
+                        setState(() {
+
+                        });
+                      },
                     ),
-                  ),
-                ],
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter PassWord",
+                        labelText: "Password",
+                      ),
+                      validator: (value){
+                        if(value != null && value.isEmpty){
+                          return "Password cannot be empty";
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.pushNamed(context, MyRoutes.HomeRoute);
-                } ,
-                style: TextButton.styleFrom(),
-                child:Text (" Login ") )
-          ],
+              SizedBox(
+                height: 20.0,
+              ),
+
+              Builder(
+                builder: (BuildContext context) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.pushNamed(context, MyRoutes.HomeRoute);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(),
+                    child: Text("Login"),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
